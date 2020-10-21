14cb6d935986a080d7e6dbfe6d9d1fcb20bea691
Abhishek Prajapati <46470516+blackviking27@users.noreply.github.com>
2020-10-21 18:51:32 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from 'firebase';
 
+//value for all this field is available with firebase. You can create your own firebase and add the config values in order to use the app.
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBxAd8oCbLfq1FM-6oXm9_MLxi3W6Y1gaE",
-    authDomain: "instagram-clone-react-4c169.firebaseapp.com",
-    databaseURL: "https://instagram-clone-react-4c169.firebaseio.com",
-    projectId: "instagram-clone-react-4c169",
-    storageBucket: "instagram-clone-react-4c169.appspot.com",
-    messagingSenderId: "356640939200",
-    appId: "1:356640939200:web:1a22fc6e485b307cf7e125",
-    measurementId: "G-89FVGDZW26"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 });
 
 const db = firebaseApp.firestore();
